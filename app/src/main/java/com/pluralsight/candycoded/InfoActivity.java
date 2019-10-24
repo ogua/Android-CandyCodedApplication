20b9d6ecf3ddc1369138d31e4aab9baba761e477
ogua <36886781+ogua@users.noreply.github.com>
2019-10-24 07:17:58 -0700

    Update InfoActivity.java

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -41,5 +41,12 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    
+    public void createPhoneIntent(View view) {
+        Uri phoneurl = Uri.parse("tel:0123456789");
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(phoneurl);
+        startActivity(phoneIntent);
+    }
 
 }
